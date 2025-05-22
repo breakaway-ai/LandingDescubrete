0976247f48c952493ba05a31581c7a4d84c34fa2
maugarciav <mau.villanueva2002@gmail.com>
2025-05-22 14:04:34 -0600

    [ADD] Landing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' 
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: '/LandingDescubrete/', 
 })
